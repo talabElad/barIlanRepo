9103644056b8def8e4c33c3952125f373a0d1f12
wpeladmaster <drore@elad.co.il>
2024-10-13 18:44:56 +0300

    Added search ability for each screen

--- frontend/src/components/lists/InstructorsList.js
+++ frontend/src/components/lists/InstructorsList.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const InstructorsList = ({ instructors, onInstructorClick }) => (
-  <div className="list-wrap instructors">
-    <h1>Instructors List</h1>
-    <ul>
-      {instructors.map((instructor) => (
-        <li key={instructor} onClick={() => onInstructorClick(instructor)} className='item'>
-          <div className="folder-img"></div>
-          <span>{instructor}</span>
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+const InstructorsList = ({ instructors, onInstructorClick }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const filteredInstructors = instructors.filter(instructor => 
+    instructor.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div className="list-wrap instructors">
+      <h2>Instructors</h2>
+      <input 
+        type="text" 
+        placeholder="Search Instructors..." 
+        value={searchTerm} 
+        onChange={e => setSearchTerm(e.target.value)} 
+      />
+      <ul>
+        {filteredInstructors.map(instructor => (
+          <li key={instructor} onClick={() => onInstructorClick(instructor)} className='item'>
+            <div className="folder-img"></div>
+            <span>{instructor}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default InstructorsList;
--- frontend/src/components/lists/StudentsList.js
+++ frontend/src/components/lists/StudentsList.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const StudentsList = ({ students, onStudentClick }) => (
-  <div className="list-wrap students">
-    <h2>Students</h2>
-    <ul>
-      {students.map((student) => (
-        <li key={student} onClick={() => onStudentClick(student)} className='item'>
-          <div className="folder-img"></div>
-          <span>{student}</span>
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+const StudentsList = ({ students, onStudentClick }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const filteredStudents = students.filter(student => 
+    student.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div className="list-wrap students">
+      <h2>Students</h2>
+      <input 
+        type="text" 
+        placeholder="Search Students..." 
+        value={searchTerm} 
+        onChange={e => setSearchTerm(e.target.value)} 
+      />
+      <ul>
+        {filteredStudents.map(student => (
+          <li key={student} onClick={() => onStudentClick(student)} className='item'>
+            <div className="folder-img"></div>
+            <span>{student}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default StudentsList;
